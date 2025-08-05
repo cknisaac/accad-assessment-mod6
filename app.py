49906beb266786c866a69e7dcc7266e29cabd4b6
cknisaac <isaacchankn1@gmail.com>
2025-08-05 10:35:59 +0800

    adds functionality to webapp

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 # You will likely need a database e.g. DynamoDB so you might either boto3 or pynamodb
 # Additional installs here:
-import pynamodb
-import pytest
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
+import uuid
 
 app = Flask(__name__)
 
 ## Instantiate your database here:
-#
-#
-#
+class TodoItem(Model):
+    class Meta:
+        table_name = "accad-mod6-test"  
+        region = "ap-southeast-1"  
+
+    id = UnicodeAttribute(hash_key=True)
+    title = UnicodeAttribute()
+    completed = BooleanAttribute(default=False)
 
 
 @app.route("/")
@@ -17,18 +23,6 @@ def home():
     # Complete the code below
     # The todo_list variable should be returned by running a scan on your DDB table,
     # which is then converted to a list
-    from pynamodb.models import Model
-    from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
-
-    class TodoItem(Model):
-        class Meta:
-            table_name = "accad6-assessment-db"  
-            region = "ap-southeast-1"  
-
-        id = UnicodeAttribute(hash_key=True)
-        title = UnicodeAttribute()
-        completed = BooleanAttribute(default=False)
-
     
     try:
         todo_list = list(TodoItem.scan())
@@ -41,32 +35,35 @@ def home():
 
 @app.route("/add", methods=["POST"])
 def add():
-    pass
-    # title = request.form.get("title")
+    title = request.form.get("title")
     # Complete code below to create a new item in your todo list
-    
+    if title:
+        new_item = TodoItem(id = str(uuid.uuid4()), title = title, completed = False)
+        new_item.save()
 
-    # return redirect(url_for("home"))
+    return redirect(url_for("home"))
 
 
 
 @app.route("/update/<todo_id>")
 def update(todo_id):
-    pass
+    update_item = TodoItem.get(todo_id)
     # Complete the code below to update an existing item
     # For this particular app, updating just toggles the completion between True / False
+    update_item.update(actions=[TodoItem.completed.set(not update_item.completed)])
+    update_item.save()
+    print(update_item.completed)
+
+    return redirect(url_for("home"))
 
 
-    # return redirect(url_for("home"))
-
-
-@app.route("/delete/<int:todo_id>")
+@app.route("/delete/<todo_id>")
 def delete(todo_id):
-    pass
+    delete_item = TodoItem.get(todo_id)
     # Complete the code below to delete an item from the to-do list
+    delete_item.delete()
 
-
-    # return redirect(url_for("home"))
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
